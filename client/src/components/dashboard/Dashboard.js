796316dc0ee50a904a2e1311926a7a05c5a47cc0
“vishwajeetbajwan” <vsbajwan@gmail.com>
2020-05-14 17:12:27 +0530

    Update Dashboard.js

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = (getCurrentProfile, auth, profile) => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
