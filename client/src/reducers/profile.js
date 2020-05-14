2cbd2636a3522d3c8d563af453c07db4dd9346bc
“vishwajeetbajwan” <vsbajwan@gmail.com>
2020-05-14 20:03:31 +0530

    starting on the dashboard

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -15,7 +15,7 @@ export default function (state = initialState, action) {
     case GET_PROFILE:
       return {
         ...state,
-        profiles: payload,
+        profile: payload,
         loading: false,
       };
     case PROFILE_ERROR:
@@ -23,6 +23,12 @@ export default function (state = initialState, action) {
         ...state,
         error: payload,
         loading: false,
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
       };
     default:
       return state;
